2e40614641fd23d2c38448e3d89ee34cd8b05c8f
poliwop <grove.colin@gmail.com>
2023-01-11 16:52:20 -0600

    removed old test file

--- test.py
+++
@@ -1,8 +0,0 @@
-from main import invert_matrix
-
-def test_invert_matrix():
-    assert invert_matrix([[2, 0],[0,2]], 5) == [[3, 0],[0,3]]
-    assert invert_matrix([[2, 0],[0,2]], 3) == [[2, 0],[0,2]]
-
-test_invert_matrix()
-print("All tests passed")
